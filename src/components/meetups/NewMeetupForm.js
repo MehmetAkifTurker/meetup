da5c0aca772c7944053bfa62ec994b349f5f7f97
Mehmet Akif Turker <mehmetakifturker@gmail.com>
2022-06-20 00:04:28 +0300

    handling submission

--- src/components/meetups/NewMeetupForm.js
+++ src/components/meetups/NewMeetupForm.js
@@ -1,27 +1,51 @@
+import { useRef } from 'react';
 import classes from './NewMeetupForm.module.css';
 import Card from '/Users/mehmetakifturker/Documents/Projects/learning process/Acedemimind-react-course/meetup/src/components/ui/card.js';
 function NewMeetupForm() {
-  return (
+    
+    const titleInputRef = useRef();
+    const imageInputRef = useRef();
+    const descriptionInputRef = useRef();
+    const addressInputRef = useRef();
+    
+    function handleSubmit(event) {
+        event.preventDefault();
+        const enteredTitle = titleInputRef.current.value;
+        const enteredImage = imageInputRef.current.value;
+        const enteredAddress = addressInputRef.current.value;
+        const enteredDescritption = descriptionInputRef.current.value;
+
+        const meetupData = {
+            title: enteredTitle,
+            imageUrl: enteredImage,
+            address: enteredAddress,
+            description: enteredDescritption
+
+        };
+        console.log(meetupData);
+    }
+
+    return (
     <Card>
-      <form className={classes.form}>
+      <form className={classes.form} onSubmit={handleSubmit}>
         <div className={classes.control}>
             <label htmlFor='title'>Meetup Title</label>
-            <input type="text" required id='title'/>
+            <input type="text" required id='title' ref={titleInputRef}/>
         </div>
         <div className={classes.control}>
             <label htmlFor='imageUrl'>Meetup Image</label>
-            <input type="url" required id='imageUrl'/>
+            <input type="url" required id='imageUrl' ref={imageInputRef}/>
         </div>
         <div className={classes.control}>
             <label htmlFor='address'>Meetup Address</label>
-            <input type="text" required id='address'/>
+            <input type="text" required id='address' ref={addressInputRef}/>
         </div>
         <div className={classes.control}>
             <label htmlFor='description'>Meetup Description</label>
-            <textarea id='description' required rows="5" />
+            <textarea id='description' required rows="5" ref={descriptionInputRef}/>
         </div>
-        <div className={classes.action}>
-            <button type='submit'>Add Meetup</button>
+        <div className={classes.actions}>
+            <button>Add Meetup</button>
         </div>
 
       </form>
